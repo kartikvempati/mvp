882f23d05efcb7f40cbe7e09def9d70d0f1414df
Kartik <kvempati11@gmail.com>
2016-01-04 12:33:28 -0800

    Install body parser and require in server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
+var parser = require('body-parser');
 
 var app = express();
 
 
 app.listen(8000);
+
+app.get('/', function (req, res) {
+
+});
 
 module.exports = app;

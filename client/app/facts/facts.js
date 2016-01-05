d04aad773fc9be4c45088d0eba95be1ee5a84ee4
Kartik <kvempati11@gmail.com>
2016-01-05 15:29:33 -0800

    Add a link to the source

--- client/app/facts/facts.js
+++ client/app/facts/facts.js
@@ -3,7 +3,7 @@ angular.module('til.facts', [])
 .controller('FactsController', function($scope, $location, Facts) {
   $scope.facts = {};
   Facts.getFacts().then(function(data) {
-    console.log(Object.keys(data.data));
     $scope.facts.body = data.data.data.children.slice(1);
+    console.log(typeof data.data.data.children.slice(1)[0].data.url);
   });
 });
